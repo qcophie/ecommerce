7db11c821d764833a6e0202ff06e23d7e87bd00c
josh <joshqcophie@gmail.com>
2022-02-01 20:29:47 +0000

    working on orders and checkout

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('apps.products.urls')),
-    # path('api/v1/', include('apps.order.urls')),
+    path('api/v1/', include('apps.orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
